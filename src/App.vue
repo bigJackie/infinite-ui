01d6fb7d3299d0ecd48bf9647b968c9fd00f5bd4
JackieWong <2537020964@qq.com>
2023-10-22 14:23:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
